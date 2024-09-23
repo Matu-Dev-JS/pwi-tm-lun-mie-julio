6abcc7852be5c2d1ba3b477b9f1396450f0ca77d
matu.dev <matu.a.gimenez2002@gmail.com>
2024-09-23 10:27:53 -0300

    clase 27

--- proyecto-ecommerce/src/Pages/Home/Home.jsx
+++ proyecto-ecommerce/src/Pages/Home/Home.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { Footer, Header } from '../../Components'
 
 const Home = () => {
+  const URL_API_PRODUCTS = 'http://localhost:5173/data/products.json'
+
+  
   return (
     <>
         <Header/>
